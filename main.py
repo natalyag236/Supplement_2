e776ac0fc601ac70f7a44572dbb3f5ee872afca0
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-11-20 23:44:37 -0500

    doc: added documentation to calculation circle of the area

--- main.py
+++ main.py
@@ -28,6 +28,15 @@ def test_calculate_triangle_area():
     assert triangle_area (10,12) == 60.0
 
 def circle_area(radius):
+    """ 
+    Calculate the area of a circle given its radius
+
+        Args:
+            radius: the radius of the circle
+        
+        Returns:
+            the area of the circle
+    """
     return math.pi * (radius ** 2)
 
 def test_circle_area():
