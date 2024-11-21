2586fec6e2e0fb020254af2dfa88e6b0b1ac6eef
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-11-20 22:17:00 -0500

    doc: added documentation to calculation area of rectangle

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 def area (length, width):
-     return length * width
+    """ calculate the area of a rectangle.
+    Args:
+        lenght: the length of the rectangle
+        width: the width of the rectangle.
+    Returns:
+        the calculated area of the rectangle.
+    """
+    return length * width
 
 def test_calculate_rectangle_area():
     assert area (5,6) == 30
